6f13b28ecaa029266b18b869e8be9f284934f8de
Abdulfattah Sheu <elsheikh.saf@gmail.com>
2024-08-16 10:13:07 +0100

    adding the links

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className='bg-emerald-100 py-24  '>
       <div className=' align-element grid  md:grid-cols-2 items-center gap-8'>
         <article>
-          <h1 className='text-7xl font-bold tracking-wider'>I'm Abdulfattah</h1>
+          <h1 className='text-5xl font-bold tracking-wider'>I'm Abdulfattah</h1>
           <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
             Software developer
           </p>
@@ -13,13 +13,13 @@ const Hero = () => {
             Crafting Digital Experiences and Turning pixels into possibilities.
           </p>
           <div className='flex gap-x-4 mt-4'>
-            <a href='#'>
+            <a href='https://github.com/Phattah44'>
               <FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
             </a>
-            <a href='#'>
+            <a href='https://www.linkedin.com/in/abdulfattah-sheu/'>
               <FaLinkedin className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
             </a>
-            <a href='#'>
+            <a href='https://x.com/B_sheu_jnr'>
               <FaTwitterSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
             </a>
           </div>
